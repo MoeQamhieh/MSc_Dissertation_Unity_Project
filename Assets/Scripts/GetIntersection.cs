15bbde096d61c734a5371b6cf6131617b2e266e0
MoeQamhieh <45400921+MoeQamhieh@users.noreply.github.com>
2022-08-19 16:02:37 +0100

    RayHit detection added
    Some anchors need fixing
    Astrobee pose should be calculated using hit position and normal
    Pose to be published to ROS

--- Assets/Scripts/GetIntersection.cs
+++ Assets/Scripts/GetIntersection.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    public class GetIntersection : MonoBehaviour
     {
-        
+        public GameObject Controller;
+
+        Vector3 m_ReticlePos;
+        Vector3 m_ReticleNormal;
+        int m_EndPositionInLine;
+        // interface to get target point
+        ILineRenderable lineRenderable;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        void Awake()
+        {
+            lineRenderable = Controller.GetComponent<ILineRenderable>();
+            Debug.Log("line: " + lineRenderable);
+        }
+
+        public void FindPoint()
+        {
+            lineRenderable.TryGetHitInfo(out m_ReticlePos, out m_ReticleNormal, out m_EndPositionInLine, out var isValidTarget);
+            Debug.Log("point hit: " + m_ReticlePos);
+            Debug.Log("normal " + m_ReticleNormal);
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
