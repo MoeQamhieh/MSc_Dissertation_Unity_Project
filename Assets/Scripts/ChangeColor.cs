1fc034b798c977835967c2f1897479357cadbbd6
MoeQamhieh <45400921+MoeQamhieh@users.noreply.github.com>
2022-08-24 11:35:11 +0100

    Added interaction tweaks

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public GameObject colorHandler; 
+
+    public void ChangeTo()
+    {
+        colorHandler.SetActive(true);
+    }
+
+    public void ChangeBack()
+    {
+        colorHandler.SetActive(false);
+    }
+}
